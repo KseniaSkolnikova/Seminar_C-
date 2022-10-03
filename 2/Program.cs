20865732e0c38368f3932fa78229abe03ca998f7
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-03 20:37:49 +0500

    Решили вторую задачу из домашней работы

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine("{0} наибольше число", a);
+}
+else
+{
+    Console.WriteLine("{0} наибольше число", b);
+}
