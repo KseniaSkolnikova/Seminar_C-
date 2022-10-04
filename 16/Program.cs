ccbc103b8dd60efe077f33c5a208297a124ac1f3
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-05 01:27:53 +0500

    Решили задачу 16

---
+++ 16/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+// 5,25 -> да
+// -4,16 -> да
+// 25,5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Проверка квадратного корня");
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a * a == b || b * b == a)
+{
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("нет");
+}
