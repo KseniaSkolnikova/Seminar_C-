de318f320e3b4aff3443dd97b7e30d2db891c612
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-04 23:14:34 +0500

    Решили задачу 12

---
+++ 12/Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a % b == 0)
+{
+    Console.Write("Кратное");
+}
+else
+{
+    Console.Write($"Остаток {a % b}");
+}
