caf8131fecc7b78f5b12dc102f13a5f2194054cc
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-04 14:15:31 +0500

    ПЕРЕРЕШАЛИ ЧЕТВЁРТУЮ задачу из домашней работы

--- 4/Program.cs
+++ 4/Program.cs
@@ -7,20 +7,10 @@ Console.WriteLine("Введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите третье число: ");
 int c = int.Parse(Console.ReadLine()!);
+int max = a;
 
-if (a > b)
-{
-    Console.WriteLine("{0} наибольше число", a);
-}
-else if (b < c)
-{
-    Console.WriteLine("{0} наибольше число", c);
-}
-else if (b > c)
-{
-    Console.WriteLine("{0} наибольше число", b);
-}
-else if (a > c)
-{
-    Console.WriteLine("{0} наибольше число", a);
-}
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
