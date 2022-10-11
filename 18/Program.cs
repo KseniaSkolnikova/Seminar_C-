3b1e9d18a9cdfc04f383acb71870934843817b98
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-11 21:43:09 +0500

    ПЕРЕРЕШАЛИ с кейсами 18  задачу

--- 18/Program.cs
+++ 18/Program.cs
@@ -2,22 +2,34 @@
 // возможных координат точек в этой четверти (х и у)
 
 Console.Clear();
-Console.WriteLine("Введите номер четверти: ");
+Console.WriteLine("Введите номер от 1 до 4: ");
 int num = int.Parse(Console.ReadLine()!);
 
-if (num ==1)
+switch (num)
 {
-    Console.WriteLine("x>0,y>0");
-}
-if (num == 2)
-{
-    Console.WriteLine("x<0,y>0");
-}
-if (num == 4)
-{
-    Console.WriteLine("x>0,y<0");
-}
-if (num == 3)
-{
-    Console.WriteLine("x<0,y<0");
+    case 1:
+        {
+            Console.WriteLine("x>0 и y>0");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("x<0 и y>0");
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine("x>0, и y<0");
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine("x<0 и y<0");
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("Введите заданное число от 1 до 4: ");
+            break;
+        }
 }
