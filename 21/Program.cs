6382211c07f0696cc9edfc9e7a4118e5e36dbe23
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-11 20:19:03 +0500

    ПЕРЕРЕШАЛИ СЛОЖНЕЕ  21 задачу

--- 21/Program.cs
+++ 21/Program.cs
@@ -3,23 +3,26 @@
 // A (3,6); B (2,1) -> 5,09 
 // A (7,-5); B (1,-1) -> 7,21
 
-
+double Dist(int x1, int y1, int x2, int y2)
+{
+    double result;
+// result=Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+// result=Math.Round(result,2);    
+    return result;
+}
 
 Console.Clear();
-Console.WriteLine("Введите x1: ");
+Console.WriteLine("Введите x координаты точки А: ");
 int x1 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите y1: ");
+Console.WriteLine("Введите y координаты точки А: ");
 int y1 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите x2: ");
+Console.WriteLine("Введите x координаты точки В: ");
 int x2 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите y2: ");
-
+Console.WriteLine("Введите y координаты точки В: ");
 int y2 = int.Parse(Console.ReadLine()!);
-int d1 = x1-x2;
-int d2 = y1-y2;
 
-double d = Math.Sqrt(d1*d1+d2*d2);
-Console.WriteLine(d);
+Console.WriteLine($"Длина отрезка АВ равна {Dist(x1, y1, x2, y2):f2}");
