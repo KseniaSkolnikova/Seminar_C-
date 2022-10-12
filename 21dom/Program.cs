d7da29076a8eb04725f4a331c713f3caffb819bd
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-12 17:48:34 +0500

    Решили задачу 21dom из домашней работы

---
+++ 21dom/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7) -> 15,84
+// A (7,-5,0); B (1,-1,9) -> 11,53
+
+double Dist(int x1, int y1, int x2, int y2, int c1, int c2)
+{
+    double result;
+// result=Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(c2-c1)*(c2-c1));
+    result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+Math.Pow(c2-c1, 2));
+// result=Math.Round(result,2);    
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите x координаты точки А: ");
+int x1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите y координаты точки А: ");
+int y1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите c координаты точки A: ");
+int c1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите x координаты точки В: ");
+int x2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите y координаты точки В: ");
+int y2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите c координаты точки B: ");
+int c2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Длина отрезка АВC равна {Dist(x1, y1, x2, y2, c1, c2):f2}");
