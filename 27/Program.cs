75044e4ae6e9e222221e6d731a1b6b1428746b0f
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-13 23:48:40 +0500

    Решили 27 задачу из домашней работы

---
+++ 27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите функцию и запустите её, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10  
+// 9012 -> 12
+
+int GetSum(int A)
+{
+    int sum = 0;
+
+    for (int i = 0; i <= A; i++)
+    {
+        sum += A % 10;
+        A = A / 10;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа равна = {GetSum(n)}");
