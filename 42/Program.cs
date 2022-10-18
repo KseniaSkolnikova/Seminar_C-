32ed0c030a831cb6fa2f5918f01905a4d5060ca9
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-18 23:32:39 +0500

    дописать задачу 42

---
+++ 42/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+string NewMass(int a)
+{
+    string arr = "";
+    while (a > 0)
+    {
+        arr += (a % 2).ToString();
+        a /= 2;
+    }
+    return arr;
+}
+
+int MassRev(string arr)
+{
+    string rezult = "";
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        rezult += arr[arr.Length-1-i];
+    }
+    return int.Parse(rezult);
+}
