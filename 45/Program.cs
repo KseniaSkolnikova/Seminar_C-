f213409ab50adf26f5a2d7c71e425e8fcf16e45e
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-19 00:04:54 +0500

    Решили 45 задачу

---
+++ 45/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 45: 
+// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного копирования.
+
+void ReversArray1(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int temp = inArray[i];
+        inArray[i] = inArray[inArray.Length - 1 - i];
+        inArray[inArray.Length - 1 - i] = temp;
+    }
+}
+
+int[] NewArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[i];
+    }
+    return result;
+}
+
