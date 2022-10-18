daec051510416db58b6e8fc353e53ef6e4573c14
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-18 19:25:49 +0500

    Решили 34 задачу из домашней работы

---
+++ 34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] array = randomArray(4);
+
+int[] randomArray(int size)                   // массив трёхзначных чисел
+{
+    Random random = new Random();
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+    return array;
+}
+
+
+int countOfEven(int[] array)              // ищем чётные числа
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine($"[{string.Join(",", array)}] -> чётных чисел {countOfEven(array)}");
