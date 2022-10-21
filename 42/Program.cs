ee5e9945470e41874296225ae09a2534b7c2c908
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-21 18:30:43 +0500

    Решили 42 задачу

--- 42/Program.cs
+++ 42/Program.cs
@@ -10,13 +10,13 @@ string NewMass(int a)
     string arr = "";
     while (a > 0)
     {
-        arr += (a % 2).ToString();
+        arr += (a % 2).ToString();  // превращает в строку
         a /= 2;
     }
     return arr;
 }
 
-int MassRev(string arr)
+int MassRev(string arr)  // целое число
 {
     string rezult = "";
 
@@ -24,5 +24,13 @@ int MassRev(string arr)
     {
         rezult += arr[arr.Length-1-i];
     }
-    return int.Parse(rezult);
+    return int.Parse(rezult);  // строку превращаем в целое число
 }
+
+Console.Clear();
+Console.WriteLine("Введите десятичное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+string array = NewMass(num);
+int num1 = MassRev(array);
+Console.WriteLine($"В двоичном виде: {num1}");
