22be5f0af15adedd47ea5156fd7cec1d39872aad
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-24 21:43:24 +0500

    Дописали решение 45 задачи

--- 45/Program.cs
+++ 45/Program.cs
@@ -2,6 +2,18 @@
 // Напишите программу, которая будет создавать
 // копию заданного массива с помощью поэлементного копирования.
 
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
 
 int[] NewArray2(int[] inArray)
 {
@@ -13,3 +25,12 @@ int[] NewArray2(int[] inArray)
     }
     return result;
 }
+
+Console.Clear();
+Console.WriteLine("Введите массив через пробел: ");
+string input = Console.ReadLine();
+int[] numArray = GetArrayFromString(input);
+
+int[] newArray = NewArray2 (numArray);
+
+Console.WriteLine(String.Join(", ", newArray));
