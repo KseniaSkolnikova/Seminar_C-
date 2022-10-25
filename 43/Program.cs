be78f5c90236bd112b4f57227f7a30bf3514c96c
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-25 16:20:41 +0500

    ПЕРЕРЕШАЛИ сложнее 43 задачу из домашней работы

--- 43/Program.cs
+++ 43/Program.cs
@@ -5,8 +5,27 @@
 // b1=2, k1=5, b2=4, k2=9  -> (-0,5; -0,5)
 
 
-Console.Clear();
+void Across(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2 && b1 == b2)
+    {
+        Console.WriteLine($"Прямые совпадают");
+    }
+    else if (k1 == k2)
+    {
+        Console.WriteLine($"Прямые параллельны");
+    }
+    else
+    {
+        double x = (-b2 + b1) / (-k1 + k2);
+        double y = k2 * x + b2;
+        x = Math.Round(x, 2);
+        y = Math.Round(y, 2);
+        Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+    }
+}
 
+Console.Clear();
 Console.WriteLine("введите значение b1");
 double b1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число k1");
@@ -16,9 +35,4 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число k2");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (-b2 + b1)/(-k1 + k2);
-double y = k2 * x + b2;
-
-Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
-
-
+Across(b1, k1, b2, k2);
