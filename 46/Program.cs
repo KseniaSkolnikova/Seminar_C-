8a56b9fddc51793f7a1613b95ce1aeb51de72b7a
KseniaShkolnikova <naxalu85@gmail.com>
2022-10-26 18:59:20 +0500

    Решили 46 задачу с комментариями

--- 46/Program.cs
+++ 46/Program.cs
@@ -5,14 +5,14 @@
 // 5 -2 33 -2
 // 77 3 8 1
 
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+int[,] GetArray(int m, int n, int minValue, int maxValue)  // константа3 для двумерного массива [,]
 {
     int[,] result = new int[m, n];
 
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
+    for (int i = 0; i < m; i++)        // строки
+        for (int j = 0; j < n; j++)    // столбцы
         {
-            result[i, j] = new Random().Next(minValue, maxValue+1);
+            result[i, j] = new Random().Next(minValue, maxValue+1);   // случайные числа
         }
     
     return result;
@@ -20,11 +20,11 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
 
 void PrintArray(int[,] inArray)
 {
-    for (int i = 0; i < inArray.GetLength(0); i++)
+    for (int i = 0; i < inArray.GetLength(0); i++)   // дина массива, строки
     {
-        for (int j = 0; j < inArray.GetLength(1); j++)
+        for (int j = 0; j < inArray.GetLength(1); j++)   // длина массива, столбцы
         {
-            Console.Write($"{inArray[i, j]}\t ");
+            Console.Write($"{inArray[i, j]}\t ");   // красивый вид, табуляция
         }
         Console.WriteLine();
     }
@@ -32,9 +32,9 @@ void PrintArray(int[,] inArray)
 
 Console.Clear();
 Console.Write("Введите количество строк в массиве: ");
-int row = int.Parse(Console.ReadLine()!);
+int row = int.Parse(Console.ReadLine()!);                    // строки
 Console.Write("Введите количество столбцов в массиве: ");
-int columns = int.Parse(Console.ReadLine()!);
+int columns = int.Parse(Console.ReadLine()!);                // столбцы
 
 int[,] array = GetArray(row, columns, 0, 10);
-PrintArray(array);
+PrintArray(array);                                          // печатает двумерный массив
