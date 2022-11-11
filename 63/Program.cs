e9edd9ff4ce766ca7ed6cf863ac419695fcc3a7a
KseniaShkolnikova <naxalu85@gmail.com>
2022-11-11 22:46:41 +0500

    Криво написанная 63 задача

---
+++ 63/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+void PrintNumber(int n)
+{
+    if (n < 1) return;
+    PrintNumber(n - 1);
+    Console.Write(n + " ");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+
+Console.Clear();
+int n = InputNumbers("Введите n: ");
+PrintNumber(n);
